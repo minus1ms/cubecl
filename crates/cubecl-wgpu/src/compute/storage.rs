df40f96f1a96edecbfb30c6aea250084c51dcd25
minus1ms <qwertyytpl1612@gmail.com>
2025-02-17 14:38:11 +0100

    test

--- crates/cubecl-wgpu/src/compute/storage.rs
+++ crates/cubecl-wgpu/src/compute/storage.rs
@@ -88,6 +88,10 @@ impl ComputeStorage for WgpuStorage {
     }
 
     fn alloc(&mut self, size: u64) -> StorageHandle {
+        if size == 2147483648 {
+            todo!("Size: {:?}", size);
+        }
+
         let id = StorageId::new();
 
         let buffer = self.device.create_buffer(&wgpu::BufferDescriptor {
